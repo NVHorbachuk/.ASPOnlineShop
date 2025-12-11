fcc8f455ea6630eb7d80cd013dd134588394bd6b
NVHorbachuk <horbachuknazar@gmail.comgit config --global user.email>
2025-12-11 22:53:33 +0200

    Ad Db and other tools

---
+++ DATA/Class.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OnlineShop.Models;
+
+namespace OnlineShop.Data
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                
+                if (context.Products.Any())
+                {
+                    return;
+                }
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Patek Philippe Nautilus 5711",
+                        Price = 135000.00M,
+                        Category = "Luxury Sport",
+                        Stock = 1
+                    },
+                    new Product
+                    {
+                        Name = "Rolex Submariner Date",
+                        Price = 16500.00M,
+                        Category = "Diver Watch",
+                        Stock = 3
+                    },
+                    new Product
+                    {
+                        Name = "Audemars Piguet Royal Oak",
+                        Price = 45000.00M,
+                        Category = "Luxury Mechanical",
+                        Stock = 2
+                    },
+                    new Product
+                    {
+                        Name = "Vacheron Constantin Overseas",
+                        Price = 32000.00M,
+                        Category = "Haute Horlogerie",
+                        Stock = 1
+                    },
+                    new Product
+                    {
+                        Name = "A. Lange & Söhne Lange 1",
+                        Price = 48000.00M,
+                        Category = "Dress Watch",
+                        Stock = 2
+                    }
+                );
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using OnlineShop.Data;
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    SeedData.Initialize(services);
+}
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    // https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
